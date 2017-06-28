0bc09e2339653ff081e6dde1380ae7272b65d226
shizhe <13193488125@163.com>
2017-06-28 21:57:11 +0800

    6-28-21:56commit

--- firstMaster.java
+++ firstMaster.java
@@ -1,11 +1,8 @@
 
-public class FirstGitTest {
+public class firstMaster {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-			System.out.println("6-28-´´½¨¡£");
-		
 
 	}
 
